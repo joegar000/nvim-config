978a948b3f6f9d09745070213d6bc5f12519ff4a
David Garcia <joegar000@gmail.com>
2023-12-16 16:31:42 -0600

    Replace vim-sleuth with guess-indent

--- lua/myconfig/packer.lua
+++ lua/myconfig/packer.lua
@@ -11,20 +11,20 @@ return require('packer').startup(function(use)
 
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    -- or, branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   use 'Mofiqul/vscode.nvim'
 
 
   use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = function()
-		  local ts_update = require('nvim-treesitter.install').update({with_sync = true})
-		  ts_update()
-	  end
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({with_sync = true})
+      ts_update()
+    end
   }
 
   use('nvim-treesitter/playground')
@@ -36,20 +36,20 @@ return require('packer').startup(function(use)
   use('tpope/vim-fugitive')
 
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment these if you want to manage LSP servers from neovim
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      --- Uncomment these if you want to manage LSP servers from neovim
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
 
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-	  }
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
   }
 
   use('kylechui/nvim-surround')
@@ -69,7 +69,10 @@ return require('packer').startup(function(use)
 
   use('preservim/nerdcommenter')
 
-  use('tpope/vim-sleuth')
+  use {
+    'nmac427/guess-indent.nvim',
+    config = function() require('guess-indent').setup {} end,
+  }
 
   -- Plugins end here
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -94,6 +94,12 @@ _G.packer_plugins = {
     path = "/Users/davidgarcia/.local/share/nvim/site/pack/packer/start/emmet-vim",
     url = "https://github.com/mattn/emmet-vim"
   },
+  ["guess-indent.nvim"] = {
+    config = { "\27LJ\2\n>\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\17guess-indent\frequire\0" },
+    loaded = true,
+    path = "/Users/davidgarcia/.local/share/nvim/site/pack/packer/start/guess-indent.nvim",
+    url = "https://github.com/nmac427/guess-indent.nvim"
+  },
   harpoon = {
     loaded = true,
     path = "/Users/davidgarcia/.local/share/nvim/site/pack/packer/start/harpoon",
@@ -194,11 +200,6 @@ _G.packer_plugins = {
     path = "/Users/davidgarcia/.local/share/nvim/site/pack/packer/start/vim-jsx-pretty",
     url = "https://github.com/maxmellon/vim-jsx-pretty"
   },
-  ["vim-sleuth"] = {
-    loaded = true,
-    path = "/Users/davidgarcia/.local/share/nvim/site/pack/packer/start/vim-sleuth",
-    url = "https://github.com/tpope/vim-sleuth"
-  },
   ["vscode.nvim"] = {
     loaded = true,
     path = "/Users/davidgarcia/.local/share/nvim/site/pack/packer/start/vscode.nvim",
@@ -212,6 +213,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: guess-indent.nvim
+time([[Config for guess-indent.nvim]], true)
+try_loadstring("\27LJ\2\n>\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\17guess-indent\frequire\0", "config", "guess-indent.nvim")
+time([[Config for guess-indent.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
