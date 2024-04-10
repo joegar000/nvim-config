f2061b0315561f0b9e429de61fe55e6da1a0e75a
David Garcia <joegar000@gmail.com>
2024-04-09 21:30:31 -0500

    Use new auto pairs plugin

---
+++ lua/plugins/auto-pairs.lua
@@ -0,0 +1,26 @@
+return {
+  "jiangmiao/auto-pairs",
+  lazy = false,
+  keys = {
+    {
+      '<leader>up',
+      function()
+        local notify = require('notify').notify
+        if vim.b.autopairs_enabled == 1 then
+          notify('Disabled auto pairs', vim.log.levels.WARN, { title = 'auto-pairs' })
+          vim.b.autopairs_enabled = 0
+        else
+          notify('Enabled auto pairs', vim.log.levels.INFO, { title = 'auto-pairs' })
+          vim.b.autopairs_enabled = 1
+        end
+      end,
+      desc = 'Toggle auto pairs'
+    }
+  },
+  config = function()
+    vim.g.AutoPairsShortcutToggle = ''
+    vim.g.AutoPairsShortcutFastWrap = ''
+    vim.g.AutoPairsShortcutJump = ''
+    vim.g.AutoPairsShortcutBackInsert = ''
+  end
+}
