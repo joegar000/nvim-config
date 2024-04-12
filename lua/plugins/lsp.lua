53dbb8fd945df1c0ea4c9ffa57d79472f8c0ea6e
David Garcia <joegar000@gmail.com>
2024-04-12 06:56:42 -0500

    Finally make lsp like what it used to be

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,12 +3,14 @@ return {
   {
     "folke/neodev.nvim",
     lazy = false,
-    opts = {}
+    config = function()
+      require('neodev').setup()
+    end
   },
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
-    lazy = false,
+    lazy = true,
     keys = {
       { '<leader>gd', function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition" },
       { '<leader>gD', vim.lsp.buf.declaration, desc = "Goto Declaration" },
@@ -41,12 +43,6 @@ return {
       { "<leader>cc", vim.lsp.codelens.run, desc = "Run Codelens", mode = { "n", "v" } },
       { "<leader>cC", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", mode = { "n" } }
     },
-    config = function()
-      require('neodev').setup() -- Load neodev before any other lsp plugins
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.setup()
-      lsp_zero.extend_lspconfig()
-    end,
     init = function()
       local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
       vim.opt.rtp:prepend(lazypath)
@@ -76,43 +72,76 @@ return {
     lazy = false,
     keys = {
       { '<leader>m', '<cmd>Mason<CR>', desc = 'Open Mason' }
-    }
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    lazy = false
-  },
-  {
-    'neovim/nvim-lspconfig',
-    lazy = false
-  },
-  {
-    'hrsh7th/cmp-nvim-lsp',
-    lazy = false
+    },
+    config = true
   },
   {
     'hrsh7th/nvim-cmp',
     lazy = false,
+    dependencies = {
+      {'L3MON4D3/LuaSnip'},
+    },
     config = function()
+      local lsp = require('lsp-zero')
+      lsp.extend_cmp()
+
       local cmp = require('cmp')
+      local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
       cmp.setup({
         sources = {
           {name = 'path'},
           {name = 'nvim_lsp'},
           {name = 'nvim_lua'},
         },
+        formatting = lsp.cmp_format(),
         mapping = cmp.mapping.preset.insert({
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
           ['<C-y>'] = cmp.mapping.confirm({ select = true }),
           ['<C-Space>'] = cmp.mapping.complete(),
         })
       })
     end
-    },
-  {
-    'L3MON4D3/LuaSnip',
-    lazy = false
   },
+  {
+    'neovim/nvim-lspconfig',
+    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason-lspconfig.nvim'},
+    },
+    config = function()
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
+
+      -- if you want to know more about mason.nvim
+      -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+      lsp_zero.on_attach(function(client, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+
+      require('mason-lspconfig').setup({
+        ensure_installed = {},
+        handlers = {
+          -- this first function is the "default handler"
+          -- it applies to every language server without a "custom handler"
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+
+          -- this is the "custom handler" for `lua_ls`
+          lua_ls = function()
+            -- (Optional) Configure lua language server for neovim
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+          end,
+        }
+      })
+    end
+  }
 }
 
