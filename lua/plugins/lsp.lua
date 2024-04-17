051a7a79f2951fd9c3c87fd10642d25235035f7e
joegar000 <69176505+joegar000@users.noreply.github.com>
2024-04-17 10:18:53 -0500

    Update lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,7 @@ return {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     lazy = true,
+    config = false,
     keys = {
       { '<leader>gd', function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition" },
       { '<leader>gD', vim.lsp.buf.declaration, desc = "Goto Declaration" },
@@ -44,40 +45,21 @@ return {
       { "<leader>cC", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", mode = { "n" } }
     },
     init = function()
-      local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-      vim.opt.rtp:prepend(lazypath)
-
-      vim.cmd.colorscheme('habamax')
-
-      local lsp_zero = require('lsp-zero')
-
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({buffer = bufnr})
-      end)
-
-      -- to learn how to use mason.nvim with lsp-zero
-      -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-      require('mason').setup({})
-      require('mason-lspconfig').setup({
-        handlers = {
-          lsp_zero.default_setup,
-        }
-      })
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
     end
   },
   {
     'williamboman/mason.nvim',
     lazy = false,
+    config = true,
     keys = {
       { '<leader>m', '<cmd>Mason<CR>', desc = 'Open Mason' }
-    },
-    config = true
+    }
   },
   {
     'hrsh7th/nvim-cmp',
-    lazy = false,
+    event = 'InsertEnter',
     dependencies = {
       {'L3MON4D3/LuaSnip'},
     },
@@ -87,6 +69,7 @@ return {
 
       local cmp = require('cmp')
       local cmp_select = {behavior = cmp.SelectBehavior.Select}
+      local cmp_action = lsp.cmp_action()
 
       cmp.setup({
         sources = {
@@ -98,9 +81,18 @@ return {
         mapping = cmp.mapping.preset.insert({
           ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
           ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
           ['<C-y>'] = cmp.mapping.confirm({ select = true }),
           ['<C-Space>'] = cmp.mapping.complete(),
-        })
+        }),
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
       })
     end
   },
