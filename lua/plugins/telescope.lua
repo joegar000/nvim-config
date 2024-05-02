b8ebfb35c4592d3d4bcafcc9c65989b877c212e9
David Garcia <joegar000@gmail.com>
2024-05-02 15:59:13 -0500

    Fix telescope keymaps command and add command for viewing notification history

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,7 +19,8 @@ return {
         { '<leader>tf', '<cmd>Telescope find_files<CR>', desc = 'Find files' },
         { '<leader>tg', '<cmd>Telescope git_files<CR>', desc = 'Git files' },
         { '<leader>tlg', '<cmd>Telescope live_grep<CR>', desc = 'Live grep' },
-        { '<leader>tk', '<cmd>Telescope keymaps', desc = 'Keymaps' }
+        { '<leader>tk', '<cmd>Telescope keymaps<CR>', desc = 'Keymaps' },
+        { '<leader>tn', '<cmd>Telescope notify<CR>', desc = 'Nofication history' }
     },
     cmd = 'Telescope',
     config = true,
