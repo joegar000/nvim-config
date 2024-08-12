c58f9dbda7ecb39f83e024dff01ac04a99a38460
David Garcia <joegar000@gmail.com>
2024-08-11 21:35:46 -0500

    Don't automatically center screen on <C-(d|u)>

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -11,8 +11,8 @@ vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- Keeps cursor in center of screen when using C-d or C-u
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Keeps cursor in center of screen when using search
 vim.keymap.set("n", "n", "nzzzv")
