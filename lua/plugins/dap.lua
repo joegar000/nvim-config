6854b67d737b44796ba00c1621c4ad04a7c8d661
joegar000 <joegar000@gmail.com>
2024-08-15 13:20:42 -0500

    Dap updates

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -65,14 +65,16 @@ return {
           "linux-cultist/venv-selector.nvim",
           dependencies = {
             "neovim/nvim-lspconfig",
-            "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+            "mfussenegger/nvim-dap",
+            "mfussenegger/nvim-dap-python", --optional
             { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
           },
           lazy = false,
           branch = "regexp", -- This is the regexp branch, use this for the new version
           config = function()
             require('venv-selector').setup({
-              pipenv_path = IsWindows and '~\\.virtualenvs\\' or '~/.local/share/virtualenvs/'
+              pipenv_path = IsWindows and '~\\.virtualenvs\\' or '~/.local/share/virtualenvs/',
+              dap_enabled = true
             })
           end,
           keys = {
