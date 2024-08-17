6055eceaabc90f82fddd71925f90277bdd471f68
David Garcia <joegar000@gmail.com>
2024-08-17 17:33:35 -0500

    Add bug workaround and set background color to match foreground color

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -3,14 +3,17 @@ if not IsWindows then
     "mistricky/codesnap.nvim",
     build = "make build_generator",
     keys = {
-      { "<leader>cs", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
-      { "<leader>cp", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+      { "<leader>cs", "<Esc><cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+      { "<leader>cp", "<Esc><cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
     },
     opts = {
       has_breadcrumbs = true,
-      bg_color = "#535c68",
       watermark = "",
-      mac_window_bar = false
+      mac_window_bar = false,
+      bg_x_padding = 1,
+      bg_y_padding = 1,
+      bg_color = "#292D34",
+      has_line_number = true
     }
   }
 else
