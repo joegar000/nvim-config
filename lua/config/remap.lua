2375523e5a97a415ed0d40b905471f8401c1d370
joegar000 <joegar000@gmail.com>
2024-09-18 10:12:47 -0500

    Remove some remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -8,15 +8,15 @@ vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
 -- Keeps cursor in same position when appending lines to current lines when using J
-vim.keymap.set("n", "J", "mzJ`z")
+-- vim.keymap.set("n", "J", "mzJ`z")
 
 -- Keeps cursor in center of screen when using C-d or C-u
 -- vim.keymap.set("n", "<C-d>", "<C-d>zz")
 -- vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Keeps cursor in center of screen when using search
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+-- vim.keymap.set("n", "n", "nzzzv")
+-- vim.keymap.set("n", "N", "Nzzzv")
 
 -- Don't know why, but primeagen doesn't like capital q
 vim.keymap.set("n", "Q", "<nop>")
